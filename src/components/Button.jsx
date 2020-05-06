90b6a832a35e839ad0fae2bd580132a040e20eac
Aecio Silva <aeciofcs@gmail.com>
2020-05-06 12:14:56 -0300

    Ajustes no Comp Button e adicionado as funções

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,6 +3,12 @@ import './Button.css'
 
 //criando componentes sem estado.
 export default props => 
-    <button className="button">
+    <button className={`
+        button
+        ${props.operation ? 'operation' : ''}
+        ${props.double ? 'double' : ''}
+        ${props.triple ? 'triple' : ''}        
+    `} 
+        onClick={e => props.click && props.click(props.label)} >
         {props.label}
     </button>
