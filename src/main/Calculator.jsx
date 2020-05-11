553e9280382c7b9047cbaf25d6d28571fff30e8f
Aecio Silva <aeciofcs@gmail.com>
2020-05-11 16:22:44 -0300

    Retirando a função EVAL

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -16,31 +16,45 @@ export default class Calculator extends Component {
 
     state = { ...initialState }
 
-    clearMemory(){
-        this.setState({...initialState})
+    clearMemory(){        
+        this.setState({...initialState})        
     }
 
     setOperation(operation){
-        if (this.state.current === 0) {
+        if (this.state.current === 0) {            
             this.setState({ operation, current: 1, clearDisplay: true })
         }else{
             const equals = operation === '='
             const currentOperation = this.state.operation
             const values = [...this.state.values]
             try{
-                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+                switch (currentOperation) {
+                    case "+":
+                        values[0] = values[0] + values[1]
+                        break;
+                    case "-":
+                        values[0] = values[0] - values[1]
+                        break;
+                    case "*":
+                        values[0] = values[0] * values[1]
+                        break;
+                    case "/":
+                        values[0] = values[0] / values[1]
+                        break;
+                    default:
+                        break;
+                }              
             } catch{
                 values[0] = this.state.values[0]
             }
             values[1] = 0
-
             this.setState({ 
                 displayValue: values[0],
                 operation: equals ? null : operation,
                 curent: equals ? 0 : 1,
                 clearDisplay: !equals,
                 values
-            })
+            })            
         }
     }
 
@@ -53,26 +67,26 @@ export default class Calculator extends Component {
             || this.state.clearDisplay
         const currentValue = clearDisplay ? '' : this.state.displayValue
         const displayValue = currentValue + num
+        
         this.setState({ displayValue, clearDisplay: false  })
 
         if (num !== '.') {
             const i = this.state.current
             const newValue = parseFloat(displayValue)
-            const values = [...this.state.values]
+            const values = [...this.state.values]            
             values[i] = newValue
             this.setState({ values })
         }
     }
 
-
     render() {
         const addDigit = num => this.addDigit(num)
-        const setOperation = op => this.setOperation(op)
-
+        const setOperation = op => this.setOperation(op)        
         return (
             <div className="calculator">
-                <Display value={this.state.displayValue} />
-                <Button label="AC" triple click={ () => this.clearMemory() } />
+                <Display value={this.state.displayValue } />
+                <Button label="AC" double click={ () => this.clearMemory() } />
+                <Button label="%" />
                 <Button label="/" operation click={setOperation} />
                 <Button label="7" click={addDigit}/>
                 <Button label="8" click={addDigit}/>
